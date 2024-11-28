687d540c3ccfd95e75b3f06c419bb8526a162c1c
maxwel kirimi <maxwelkirimii@gmail.com>
2024-11-28 13:59:14 +0300

    assignment

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,43 @@
-#!/usr/bin/env python3
-
+# admin_login function
 def admin_login(username, password):
-    # your code here
-    pass
+    if username.lower() == "admin" and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
 
-def hows_the_weather(temperature):
-    # your code here
-    pass
 
+# hows_the_weather function
+def hows_the_weather(temp):
+    if temp < 40:
+        return "It's brisk out there!"
+    elif temp < 65:
+        return "It's a little chilly out there!"
+    elif temp < 85:
+        return "It's perfect out there!"
+    else:
+        return "It's too dang hot out there!"
+
+# fizzbuzz function
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
-def calculator(operation, num1, num2):
-    # your code here
-    pass
+# calculator function
+def calculator(operation, a, b):
+    if operation == "+":
+        return a + b
+    elif operation == "-":
+        return a - b
+    elif operation == "*":
+        return a * b
+    elif operation == "/":
+        return a / b
+    else:
+        print("Invalid operation!")
+        return None
